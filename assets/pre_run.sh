517beac828b764a250521eefe62d74655def95ba
Yifan Gao <git@gaoyifan.com>
2016-07-04 05:08:45 +0800

    run init database script at every restart time

--- assets/pre_run.sh
+++ assets/pre_run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-INIT_DATABASE_FLAG=$ASSETS_DIR/init-databse.flag
-
 # running at the very first time
-if [[ ! -f $INIT_DATABASE_FLAG ]] && [ $MYSQL_ADMIN_USERNAME ] && [[ $INIT_DATABASE_ENABLE != false ]]; then
+if [ $MYSQL_ADMIN_USERNAME ] && [[ $INIT_DATABASE_ENABLE != false ]]; then
     cat /srv/{tables.sql,user.sql} | mysql -h$MYSQL_HOST -u$MYSQL_ADMIN_USERNAME -p$MYSQL_ADMIN_PASSWORD
-    touch $INIT_DATABASE_FLAG
 fi
